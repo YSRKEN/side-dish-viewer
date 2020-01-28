978e6e18633e989304af73d188089a25480d7d00
YSRKEN <ysr.ken@gmail.com>
2020-01-28 16:47:04 +0900

    [add] クライアント側に必要なライブラリをインストールした

--- client/side-dish-viewer/src/App.tsx
+++ client/side-dish-viewer/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row my-3">
+        <div className="col">
+          <h1 className="text-center">テスト</h1>
+        </div>
+      </div>
     </div>
   );
 }
