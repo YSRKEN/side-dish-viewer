3afa33f35ba214c305a04600e1da59b3b510b2ce
YSRKEN <ysr.ken@gmail.com>
2020-01-28 17:34:42 +0900

    [add] React-Bootstrapを導入し、タブ切り替え部分をサクッと実装した

--- client/side-dish-viewer/src/App.tsx
+++ client/side-dish-viewer/src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'App.css';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <div className="row my-3">
-        <div className="col">
-          <h1 className="text-center">テスト</h1>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row className="my-3">
+        <Col>
+          <h1 className="text-center">ビューアー</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Nav variant="tabs" defaultActiveKey="recent">
+            <Nav.Item>
+              <Nav.Link eventKey="recent">新着</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="favorite">お気に入り</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="log">ログ</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
